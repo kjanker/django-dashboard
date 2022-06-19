2ecd56403c1f1ca6288556f29a59a7c89fb3c016
kjanker <34544392+kjanker@users.noreply.github.com>
2022-06-19 14:30:35 +0200

    simple create form for models

--- apps/models/urls.py
+++ apps/models/urls.py
@@ -6,8 +6,10 @@ from . import views
 urlpatterns = [
     # ex: /models/
     path("models/", views.all_models, name="all_models"),
+    # ex: /models/new
+    path("models/new", views.new_model, name="model_details"),
     # ex: /models/5/
-    path("models/<int:strategy_id>/", views.model_details, name="model_details"),
+    path("models/<int:model_id>/", views.model_details, name="model_details"),
     # ex: /models/5/
-    path("models/<int:strategy_id>/toggle", views.toggle_model, name="toggle_model"),
+    path("models/<int:model_id>/toggle", views.toggle_model, name="toggle_model"),
 ]
--- apps/models/views.py
+++ apps/models/views.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .forms import TradingModelForm
 from .models import TradingModel
@@ -17,28 +17,56 @@ def all_models(request):
 
 
 @login_required(login_url="/login/")
-def model_details(request, strategy_id):
-    model = TradingModel.objects.filter(id=strategy_id)[0]
+def model_details(request, model_id=None):
+    model = TradingModel.objects.filter(id=model_id)[0] if model_id else None
     msg = None
+    editable = request.user == model.owner
 
-    if request.method == "POST" and request.user == model.owner:
+    if request.method == "POST" and editable:
         form = TradingModelForm(request.POST, instance=model)
         if form.is_valid():
             form.save()
             msg = "Changes to the model have been saved!"
         else:
-            msg = f"Error: Could not save model!"
+            msg = "Error: Could not save model!"
     else:
         form = TradingModelForm(instance=model)
 
-    context = {"segment": "all-models", "model": model, "form": form, "msg": msg}
+    context = {
+        "segment": "all-models",
+        "model": model,
+        "form": form,
+        "msg": msg,
+        "editable": editable,
+    }
 
     return render(request, "models/single-model.html", context)
 
 
 @login_required(login_url="/login/")
-def toggle_model(request, strategy_id):
-    model = TradingModel.objects.filter(id=strategy_id)[0]
+def new_model(request):
+    msg = None
+
+    if request.method == "POST":
+        form = TradingModelForm(request.POST, initial={"owner_id": request.user})
+        if form.is_valid():
+            model = form.save(commit=False)
+            model.owner = request.user
+            model.save()
+            return redirect("model_details", model_id=model.id)
+        else:
+            msg = "Error: Could not save model!"
+    else:
+        form = TradingModelForm()
+
+    context = {"segment": "all-models", "form": form, "msg": msg, "new": True}
+
+    return render(request, "models/single-model.html", context)
+
+
+@login_required(login_url="/login/")
+def toggle_model(request, model_id):
+    model = TradingModel.objects.filter(id=model_id)[0]
     if request.user == model.owner:
         model.active = not model.active
         model.save()
